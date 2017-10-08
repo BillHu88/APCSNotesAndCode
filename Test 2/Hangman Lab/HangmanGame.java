206dc390855bcabe96d8ae3353dfc82e14574536
prathgan <prathamgandhi4@gmail.com>
2017-10-08 16:10:54 -0400

    did hangman lab

--- Test 2/Hangman Lab/HangmanGame.java
+++ Test 2/Hangman Lab/HangmanGame.java
@@ -1,14 +1,45 @@
 public class HangmanGame{
-  private static String word;
-  private static StringBuffer soFar = new StringBuffer(Hangman.word);
-  private static StringBuffer triedLets = new StringBuffer();
-  public HangmanGame(){
+  private String word;
+  private StringBuffer triedLets = new StringBuffer();
+  private StringBuffer soFar = new StringBuffer();
+  public HangmanGame(String w){
+    word = w.toUpperCase();
     for(int i = 0; i<word.length(); i++){
-      soFar.insert(i, '-');
+      soFar.append('-');
     }
   }
-  public static void main(String[] args){
-    System.out.println(soFar.toString());
-  }
 
+  public static void main(String[] args){
+
+  }
+  public String getWord(){
+    return word;
+  }
+  public String getGuessed(){
+    return soFar.toString();
+  }
+  public String getTried(){
+    return triedLets.toString();
+  }
+  public int tryLetter(char letter){
+    char let = Character.toUpperCase(letter);
+    if((triedLets.toString()).indexOf(let)==-1){ //if this letter hasn't been tried
+      triedLets.append(let);
+      if(word.indexOf(let)==-1){ //if the guessed letter is not in the word
+        return -1;
+      }
+      else{//if guessed letter in word
+        for(int i = 0; i<word.length(); i++){
+          if(word.charAt(i)==Character.toUpperCase(letter)){
+            soFar.setCharAt(i, letter);
+          }
+        }
+        return 1;
+      }
+    }
+    else{
+      return 0;
+    }
+
+  }
 }
